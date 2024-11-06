e2f4bf4f69169a4cafe8c0e3e4a2a26c04ee7245
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-06 02:36:02 -0500

    229. Modelo - Siguiente y atender nuevo ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 
+class Ticket {
+	constructor(numero, escritorio) {
+		this.numero = numero;
+		this.escritorio = escritorio;
+	}
+}
+
 class TicketControl {
 	constructor() {
 		this.ultimo = 0;
@@ -36,6 +43,39 @@ class TicketControl {
 	guardarDB() {
 		const dbPath = path.join(__dirname, '../db/data.json');
 		fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+	}
+
+	siguiente() {
+		this.ultimo += 1;
+
+		const ticket = new Ticket(this.ultimo, null);
+
+		this.tickets.push(ticket);
+
+		this.guardarDB();
+
+		return 'Ticket ' + ticket.numero;
+	}
+
+	atenderTicket(escritorio) {
+		// no tenemos tickets
+		if (this.tickets.length === 0) {
+			return null;
+		}
+
+		const ticket = this.tickets.shift(); // this.tickets[0];
+
+		ticket.escritorio = escritorio;
+
+		this.ultimos4.unshift(ticket);
+
+		if (this.ultimos4.length > 4) {
+			this.ultimos4.splice(-1, 1);
+		}
+
+		this.guardarDB();
+
+		return ticket;
 	}
 }
 
