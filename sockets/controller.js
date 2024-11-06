7ca66fc76b07d4acfb5ad9f10982df7e064f51e5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-06 02:03:48 -0500

    228. Clase para centralizar la lógica de los tickets

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,28 +1,16 @@
+const TicketControl = require('../models/ticket-control');
 
-
+const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-    
-    console.log('Cliente conectado', socket.id );
+	socket.on('enviar-mensaje', (payload, callback) => {
+		const id = 123456789;
+		callback(id);
 
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id );
-    });
-
-    socket.on('enviar-mensaje', ( payload, callback ) => {
-        
-        const id = 123456789;
-        callback( id );
-
-        socket.broadcast.emit('enviar-mensaje', payload );
-
-    })
-
-}
-
-
+		socket.broadcast.emit('enviar-mensaje', payload);
+	});
+};
 
 module.exports = {
-    socketController
-}
-
+	socketController
+};
