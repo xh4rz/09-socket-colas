0ce2d3ee7488635a5e06412c5538d719264ee6df
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-07 00:16:19 -0500

    231. Preparar pantalla de escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,35 @@
+// referencias HTML
+const lblEscritorio = document.querySelector('h1');
 
+const btnAtender = document.querySelector('button');
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+	window.location = 'index.html';
+	throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+
+lblEscritorio.innerText = escritorio;
+
+const socket = io();
+
+socket.on('connect', () => {
+	btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+	btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+	// lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+});
+
+btnAtender.addEventListener('click', () => {
+	// socket.emit('siguiente-ticket', null, (ticket) => {
+	// 	lblNuevoTicket.innerText = ticket;
+	// });
+});
