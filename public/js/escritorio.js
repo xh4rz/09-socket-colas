0eccc1d09649412b354640d1bb70c5a7a59661b3
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-07 02:10:27 -0500

    232. Socket: Atender un ticket

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -3,6 +3,10 @@ const lblEscritorio = document.querySelector('h1');
 
 const btnAtender = document.querySelector('button');
 
+const lblTicket = document.querySelector('small');
+
+const divAlerta = document.querySelector('.alert');
+
 const searchParams = new URLSearchParams(window.location.search);
 
 if (!searchParams.has('escritorio')) {
@@ -13,6 +17,8 @@ if (!searchParams.has('escritorio')) {
 const escritorio = searchParams.get('escritorio');
 
 lblEscritorio.innerText = escritorio;
+
+divAlerta.style.display = 'none';
 
 const socket = io();
 
@@ -29,6 +35,15 @@ socket.on('ultimo-ticket', (ultimo) => {
 });
 
 btnAtender.addEventListener('click', () => {
+	socket.emit('atender-ticket', { escritorio }, ({ ok, ticket, msg }) => {
+		if (!ok) {
+			lblTicket.innerText = 'Nadie.';
+			return (divAlerta.style.display = '');
+		}
+
+		lblTicket.innerText = 'Ticket ' + ticket.numero;
+	});
+
 	// socket.emit('siguiente-ticket', null, (ticket) => {
 	// 	lblNuevoTicket.innerText = ticket;
 	// });
